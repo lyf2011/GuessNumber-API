238a93de0d7afdfc84684d6d8e0ed502b300cf52
liyaofeng <932550363@qq.com>
2020-06-22 16:11:11 +0800

    [liyaofeng]实现Controller层

---
+++ src/main/java/com/twschool/practice/controller/GameController.java
@@ -0,0 +1,20 @@
+package com.twschool.practice.controller;
+
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class GameController {
+    @PostMapping("/game/guess-numbers")
+    public Map<String,String> guess(@RequestBody Map<String, String> requestBody){
+        Map<String, String> responsetBody = new HashMap<>();
+        responsetBody.put("input",requestBody.get("number"));
+        responsetBody.put("result", "4A0B");
+        return responsetBody;
+    }
+}
